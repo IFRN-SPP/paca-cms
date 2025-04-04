bf0df5aae61d6039a00aaaa469180f877ba6529c
Diego Cirilo <dvcirilo@gmail.com>
2025-04-04 15:22:46 -0300

    novas fixtures

--- app/admin.py
+++ app/admin.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
 from .models import (
     Publication,
+    BackgroundImage,
     Issue,
     Page,
     Document,
     SocialMedia,
+    PageAllowedDocumentCategory,
 )
 
 admin.site.register(Document)
 admin.site.register(SocialMedia)
+admin.site.register(BackgroundImage)
+admin.site.register(PageAllowedDocumentCategory)
 admin.site.register(Page)
 admin.site.register(Publication)
 admin.site.register(Issue)
