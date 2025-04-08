5c53eadcf32c2332c9d0dc78e682c716477717c1
Diego Cirilo <dvcirilo@gmail.com>
2025-04-07 22:23:24 -0300

    paginação nas páginas públicas

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.http import Http404
+from django.db.models import QuerySet
 from django.views.generic.base import TemplateView
 from django.views.generic.detail import DetailView
+from django.core.paginator import Paginator
 from .models import Issue, Page, Document
 
 
@@ -20,6 +22,8 @@ class PagesDetailView(Unpublished404Mixin, DetailView):
     template_name = "page.html"
     model = Page
     context_object_name = "page"
+    issue_paginate_by = 6
+    download_paginate_by = 10
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -29,18 +33,27 @@ class PagesDetailView(Unpublished404Mixin, DetailView):
             allowed_categories = page.pagealloweddocumentcategory_set.all().values_list(
                 "category", flat=True
             )
-            content = Document.objects.filter(category__in=allowed_categories).order_by(
-                "title"
-            )
+            content = Document.objects.filter(
+                category__in=allowed_categories, publication=page.publication
+            ).order_by("title")
+            paginate_by = self.download_paginate_by
         elif page.page_type == Page.PageType.ISSUES:
             inner_template = "issues_page.html"
-            content = Issue.objects.filter(is_published=True)
+            content = Issue.objects.filter(
+                is_published=True, publication=page.publication
+            )
+            paginate_by = self.issue_paginate_by
         else:
             inner_template = "text_page.html"
             content = page.text
 
         context["inner_template"] = inner_template
-        context["content"] = content
+        if isinstance(content, (QuerySet, list, tuple)):
+            paginator = Paginator(content, paginate_by)
+            context["page_obj"] = paginator.get_page(self.request.GET.get("page"))
+            context["paginator"] = paginator
+        else:
+            context["content"] = content
         return context
 
 
