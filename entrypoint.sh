638678e2f1cf661d0d01825fad303db23d27e308
Diego Cirilo <dvcirilo@gmail.com>
2026-02-14 22:29:52 -0300

    fix socket permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,6 +22,7 @@ echo "Starting Gunicorn..."
 exec gunicorn --bind unix:/run/sockets/anais_expotec.sock \
 	--workers 3 \
 	--timeout 60 \
+    --umask 007 \
 	--access-logfile - \
 	--error-logfile - \
 	config.wsgi:application
